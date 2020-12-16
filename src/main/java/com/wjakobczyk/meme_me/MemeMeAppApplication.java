0956ac7fe926899e546ca8fe86540b752130f077
W.Jakobczyk <wiktor.jakobczyk99@gmail.com>
2020-12-16 22:28:27 +0100

    Initial commit

---
+++ src/main/java/com/wjakobczyk/meme_me/MemeMeAppApplication.java
@@ -0,0 +1,13 @@
+package com.wjakobczyk.meme_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeMeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeMeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjakobczyk/meme_me/MemeMeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjakobczyk.meme_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeMeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
